66c4dbc6b61ca43c1c0df73a4823f36a59133931
Abass Ibrahim <abassdibrahim@gmail.com>
2023-09-23 21:35:13 +0100

    by God be the glory, managed to sort out navbar drop down menu

--- webapp/src/components/Global/Navbar/Navbar.jsx
+++ webapp/src/components/Global/Navbar/Navbar.jsx
@@ -1,11 +1,50 @@
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
+
+  // State for dropdown menu
+  const [menuVisible, setMenuVisible] = useState(false)
+
+  // Menu styles
+  const menuStyle = {
+    display: menuVisible ? 'flex' : 'none',
+  };
+
+  // Event listener
+  useEffect(() => {
+    const handleResize = () => {
+      // Toggle menu visibility based on screen width
+      if (window.innerWidth <= 576) {
+        setMenuVisible(false);
+      } else {
+        setMenuVisible(true);
+      }
+    };
+
+    // Add event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+
+    // Initial check for screen width
+    handleResize();
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <nav className={styles.nav} >
       <div className={styles.container}>
-        <a href="/" className={styles.siteTitle} >DishDelve</a>
-        <ul>
+        <div className={styles.iconAndTitleContainer} >
+          <label className={styles.icon} onClick={() => { setMenuVisible(!menuVisible) }} >
+            <MenuIcon />
+          </label>
+          <a href="/" className={styles.siteTitle} >DishDelve</a>
+        </div>
+        <ul className={styles.menuItems} style={menuStyle} >
           <li>
             <a href='/' className={styles.hoverUnderlineAnimation}>Home</a>
             <a href='/' className={styles.hoverUnderlineAnimation}>About us</a>
